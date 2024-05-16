2ffe4d5034d1e6d70f9232171726a04693e986cf
Muhammad Owais <rahneverd@gmail.com>
2024-05-16 13:58:26 +0300

    smallest number in array - done

---
+++ smallest-number-in-array.js
@@ -0,0 +1,8 @@
+let arrayOfNumbers = [3,2,6,4,0,7,9]
+let smallestNumber = arrayOfNumbers[0]
+
+for (let i = 1; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] < smallestNumber) {
+        smallestNumber = arrayOfNumbers[i]
+    }
+}
